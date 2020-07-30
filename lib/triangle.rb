caef5456f95881e527b318428a54d1180f7ea377
Michael Waide <mvwaide@gmail.com>
2020-07-30 17:31:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,34 +7,33 @@ class Triangle
   end
 
   def kind
-    if kosher_triangle == true && kosher_triangle2 == true 
-      puts true 
-    else 
-      raise TriangleError 
-    end 
-    
+    if kosher_triangle == true && kosher_triangle2 == true
+      puts true
+    else
+      raise TriangleError
+    end
+
     if x == y && y == z && x == z
       :equilateral
     elsif x != y && y != z && z != x
-      :scalene 
-    else 
+      :scalene
+    else
       :isosceles
-    end 
+    end
   end
-  
-  def kosher_triangle 
+
+  def kosher_triangle
     x.positive? && y.positive? && z.positive?
-  end 
-  
-  def kosher_triangle2 
+  end
+
+  def kosher_triangle2
     x + y > z && x + z > y && y + z > x
-  end 
+  end
 end
 
 class TriangleError < StandardError
-  
+
 end
 
 a = Triangle.new(5, 5, 5)
 a.kind
-
